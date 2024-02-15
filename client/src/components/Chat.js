e6fc986c0b4ba085edf0075cddfc02cf5bec2380
sadaan <ansarisadaan72@gmail.com>
2024-02-15 22:07:15 +0530

    basic setup for user joining and use diconnecting

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import {useLocation} from 'react-router-dom';
+
+let socket;
 
 const Chat = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const location = useLocation();
+  const ENDPOINTS = "localhost:5000";
+  useEffect(()=>{
+    const {name, room} = queryString.parse(location.search);
+
+    socket = io(ENDPOINTS);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', {name, room} , ()=>{ 
+
+    });
+
+    return ()=> {
+      socket.emit('disconnect');
+      socket.off()
+    }
+  }, [location.search , ENDPOINTS]); 
   return (
     <div>Chat</div>
   )
